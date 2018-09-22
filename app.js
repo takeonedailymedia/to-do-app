df33fd7d314e6ea424abb486f91e0cc31831fd78
takeonedailymedia <cherie@takeonedaily.media>
2018-09-22 15:18:52 -0700

    slack support

--- app.js
+++ app.js
@@ -1,48 +1,28 @@
 //#1
-let id = ()
 function onReady() {
-  const toDos = [];
+  let toDos = [];
+  let id = 0;
+
   const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
 
   function createNewToDo() {
-    const newToDoText = document.getElementById('newToDoText');
-    if (!newToDoText.value) { return; }
-
     toDos.push({
-          title: newToDoText.value,
-          complete: false
-          //#2 and #3
-          id: true () =>
-    });
-    newToDoText.value = '';
-
-    renderTheUI();
-
-  }
-
+        title: newToDoText.value,
+        complete: false,
+        //#2 and #3
+        id: ++id
+  });
+}
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-
-    toDoList.textContent = '';
-
-    toDos.forEach(function(toDo) {
-      const newLi = document.createElement('li');
-      const checkbox = document.createElement('input');
+      toDoList.textContent = '';
+      toDos.forEach(function(toDo) {
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
       newLi.textContent = toDo.title;
-
-      toDoList.appendChild(newLi);
-      newLi.appendChild(checkbox);
-
-   });
-  }
-  addToDoForm.addEventListener('submit', event => {
-      event.preventDefault();
-      createNewToDo();
-    });
-
-    renderTheUI()
 
     // #4 create a delete button
     let deleteBtn = document.createElement("button");
@@ -50,17 +30,24 @@ function onReady() {
     // #4 set the button name
     deleteBtn.innerHTML = '<span>Delete</span>';
 
-    // #4 attach the deleteBtn to the li
+    // #5
+    toDoList.appendChild(newLi);
+    newLi.appendChild(checkbox);
     newLi.appendChild(deleteBtn);
 
-    // #4 add listener
     deleteBtn.addEventListener('click', function() {
+    toDoList.filter(newLi => id !== 0)
+    });
+  });
+}
+    addToDoForm.addEventListener('submit', event => {
+        event.preventDefault();
+        createNewToDo();
+        //#6
+        renderTheUI();
+    });
 
-    // #5  unsure how to create an array from this
-    
-  })
-    // #6
-    renderTheUI()
+}
 
 window.onload = function() {
   onReady();
